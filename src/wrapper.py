bdeb8f111e7c12dc2790fb1ed991b3872623b015
tct123 <42028373+tct123@users.noreply.github.com>
2025-03-29 23:24:56 +0100

    .

--- src/wrapper.py
+++ src/wrapper.py
@@ -1,11 +1,16 @@
 import requests
+
+
 class Wrapper:
     def __init__(self):
         self.url = "https://thesimpsonsquoteapi.glitch.me/quotes"
+
     def getdata(self):
         r = requests.get(url=self.url)
-        erg = r.json()[0]
-        return erg
-if __name__=="__main__":
+        result = r.json()[0]
+        return result
+
+
+if __name__ == "__main__":
     k = Wrapper()
     print(k.getdata()["quote"])
